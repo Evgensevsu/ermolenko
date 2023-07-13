5fe288c6631539c51fe6670182c633b312f6670f
Evgensevsu <eugene.grinchenko@yandex.com>
2023-07-13 21:54:28 +0300

    First commit

---
+++ Desktop/project.rb
@@ -0,0 +1,25 @@
+class SimpleCalculator
+  def add(a, b)
+    a + b
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    raise 'Cannot divide by zero' if b == 0
+
+    a / b
+  end
+end
+
+class EngineeringCalculator < SimpleCalculator
+  def power(a, b)
+    a ** b
+  end
+
